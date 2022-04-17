341dffe14c60bef18b902c5c9f1a4e57d38c0b12
LAPTOP-DTFTL919\Asus <Komchan2540@gmail.com>
2022-04-17 11:18:10 +0700

    แก้หน้าสมัคร

--- u_add.php
+++ u_add.php
@@ -13,42 +13,46 @@
     <link rel="stylesheet" href="./iconfont/material-icons.css">
     <link rel="icon" href="./img/Football Soccer Club Logo1.png">
 </head>
-<body>
+<body style="background-image: url(https://www.soccerbible.com/media/133276/mls-header-min.jpg); 
+background-position: center;
+background-repeat: no-repeat;
+background-size: cover;
+">
     <?php
         include "menuheader.php";
     ?>
 
     <div class="container">
-        <h4 class="pink-text">ลงทะเบียนผู้ใช้ใหม่</h4>
+        <h4 class="pink-text"style="text-align: center;padding: 20px ;width: 100%;margin: auto; margin-top: 50px; margin-bottom: 50px; background-color: rgba(255, 255, 255, 0.6);backdrop-filter: blur(10px);">ลงทะเบียนผู้ใช้ใหม่</h4>
         
         <form action="u_add_ok.php" method="POST">
-                <div class="row">
-                    <div class="card-panel col s12 m8 l6 offset-l3 offset-m2">
+                <div class="row ">
+                    <<div class="card-panel z-depth-4" style="width: 50%;margin: auto; margin-bottom: 50px; border-radius: 30px; background-color: rgba(255, 255, 255, 0.6);backdrop-filter: blur(10px);">
                         <div class="row">
-                            <div class="col s12  input-field">
+                            <div class="col s12  input-field ">
                                 <i class="material-icons prefix">account_box</i>
                                 <input type="text" name="u_id" id="u_id" required>
-                                <label for="u_id">ชื่อผู้ใช้(User Name)</label>
+                                <label for="u_id"class="black-text">ชื่อผู้ใช้(User Name)</label>
                             </div>
                             <div class="col s12  input-field">
                                 <i class="material-icons prefix">font_download</i>
                                 <input type="text" name="u_name" id="u_name" required>
-                                <label for="u_name">ชื่อ นามสกุล</label>
+                                <label for="u_name"class="black-text">ชื่อ นามสกุล</label>
                             </div>
                             <div class="col s12  input-field">
                                 <i class="material-icons prefix">lock</i>
                                 <input type="password" name="u_pwd" id="u_pwd" required>
-                                <label for="u_pwd">รหัสผ่าน</label>
+                                <label for="u_pwd"class="black-text">รหัสผ่าน</label>
                             </div>
                             <div class="col s12  input-field">
                                 <i class="material-icons prefix">check_box</i>
                                 <input type="password" name="u_cf_pwd" id="u_cf_pwd"  required>
-                                <label for="u_cf_pwd">ยืนยันรหัสผ่าน</label>
+                                <label for="u_cf_pwd"class="black-text">ยืนยันรหัสผ่าน</label>
                             </div>
                             <div class="col s12  input-field">
                                 <i class="material-icons prefix">chat</i>
                                 <input type="text" name="u_desc" id="u_desc">
-                                <label for="u_desc">หมายเหตุ</label>
+                                <label for="u_desc"class="black-text">หมายเหตุ</label>
                             </div>
                             <div class="col s12 right-align">
                                 <button type="submit" class="btn blue">บันทึก</button>
@@ -65,4 +69,15 @@
         include "menuscript.php";
     ?>
 </body>
+</body>
+<footer class="page-footer indigo darken-4">
+          <div class="container">
+          </div>
+          <div class="footer-copyright">
+            <div class="container">
+            © 2022 All rights reserved.
+Theme: ColorMag by FJS
+            </div>
+          </div>
+</footer>
 </html>
